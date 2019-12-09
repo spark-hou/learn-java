be214055473e432c20ddb231fc3788e902bdefdf
sparkHou <1>
2019-12-09 18:04:03 +0800

    #111

--- HelloWorld/src/work/Manage.java
+++ HelloWorld/src/work/Manage.java
@@ -3,7 +3,7 @@ package work;
 public class Manage {
     public static void main(String[] args) {
         int[] arr = new int[3];
-        arr[0]=new Employee();
+        Employee ha=new Employee();
         
     }
 
