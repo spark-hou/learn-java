7781bbf99e7c7f684b7185ff35f344bf65b4ebd9
侯欣榆 <xinyu.hou@dhc.com.cn>
2021-10-03 16:32:19 +0800

    new

---
+++ HelloWorld/src/learnClass/ArrayListLearn.java
@@ -0,0 +1,13 @@
+package learnClass;
+
+import java.util.ArrayList;
+
+public class ArrayListLearn {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("a");
+        list.add("b");
+        System.out.println(list.size());
+
+    }
+}
